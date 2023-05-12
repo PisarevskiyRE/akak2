bbd3bae91ffe3b33f5f8fde5310c84342cb5376d
Roy <daimon--@live.ru>
2023-05-13 02:01:55 +0300

    docker

--- src/main/scala/playgroud/Playground.scala
+++ src/main/scala/playgroud/Playground.scala
@@ -1,5 +1,17 @@
 package playgroud
 
-object Playground {
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import scala.concurrent.duration._
 
+object Playground {
+  def main(args: Array[String]): Unit = {
+    val system = ActorSystem(Behaviors.setup[String] { ctx =>
+      ctx.log.info("Привет!")
+      Behaviors.empty
+    }, "SimpleSystem")
+
+    import system.executionContext
+    system.scheduler.scheduleOnce(3.seconds, () => system.terminate())
+  }
 }
